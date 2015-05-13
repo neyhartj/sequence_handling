0e650a8ac454c7f6720f8c243d8ffaf68b2cb07d
Peter L. Morrell <pmorrell@x-134-84-182-221.uofm-secure.wireless.umn.edu>
2015-05-13 18:57:02 -0500

    Added torque header, comments

--- read_counts.sh
+++ read_counts.sh
@@ -4,9 +4,19 @@ set -e
 set -u
 set -o pipefail
 
+# input format for this list is simply fastq files, one per line
+# could build list with find
+# fastq_1.gz
+# fastq_2.gz
+
+#PBS -l mem=1000mb,nodes=1:ppn=1,walltime=8:00:00
+#PBS -m abe
+#PBS -M pmorrell@umn.edu
+
 # sample list
 sample_info=$1
 
+# path to bioawk installation
 bioawk=~/Apps/HLi/bioawk
 
 echo "$sample_info"
@@ -19,10 +29,12 @@ touch read_counts.txt
 
 echo ${sample_names[*]}
 
+# iterate over each of the sample names and calculate 
 for sample in ${sample_names[*]}
     do
     		count="$(bioawk -cfastx 'END{print NR}' $sample)"
     		printf %s"$sample \t $count \n"  >>./read_counts.txt
-		#$bioawk -cfastx 'END{print NR}' $sample >>./read_counts.txt
     done
+
+
 
