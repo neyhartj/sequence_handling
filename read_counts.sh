bc81dc70c34d760458c2352e81530928ed0f8197
Peter L. Morrell <pmorrell@umn.edu>
2015-05-26 22:47:30 -0500

    Added output rename, qsub

--- read_counts.sh
+++ read_counts.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
+#PBS -l mem=2000mb,nodes=1:ppn=1,walltime=8:00:00
+#PBS -m abe
+#PBS -M pmorrell@umn.edu
 
 set -e
 set -u
@@ -9,15 +12,21 @@ set -o pipefail
 # fastq_1.gz
 # fastq_2.gz
 
-#PBS -l mem=1000mb,nodes=1:ppn=1,walltime=8:00:00
-#PBS -m abe
-#PBS -M pmorrell@umn.edu
+# change to working directory
+cd ~/scratch/SCN/rename
 
 # sample list
-sample_info=$1
+# sample_info=$1
+# need to hard code the file path for qsub jobs
+sample_info=~/scratch/SCN/rename/SCN_reads.txt
 
 # path to bioawk installation
 bioawk=~/Apps/HLi/bioawk
+
+# truncate sample info file into output file name
+outfile=$(basename $sample_info .txt)
+
+echo "$outfile"
 
 echo "$sample_info"
 
@@ -25,14 +34,15 @@ echo "$sample_info"
 sample_names=($(cut -f 1 "$sample_info"))
 
 # create output file
-touch read_counts.txt
+touch ./${outfile}_out.txt
 
 echo ${sample_names[*]}
 
-# iterate over each of the sample names and calculate 
+# iterate over each of the sample names and calculate
+# read depth 
 for sample in ${sample_names[*]}
     do
     		count="$(bioawk -cfastx 'END{print NR}' $sample)"
-    		printf %s"$sample \t $count \n"  >>./read_counts.txt
+    		printf %s"$sample \t $count \n"  >>./${outfile}_out.txt
     done
 
