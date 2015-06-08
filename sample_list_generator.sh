4b4dcff4b11ff769c1e7913064f74af8973caab3
Paul Hoffman <hoffmanp@agro200953239.cfans.umn.edu>
2015-06-08 12:22:50 -0500

    New script to automatically generate a list of samples. To be used only when all samples in a directory tree are being run

---
+++ sample_list_generator.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+#   This is a simple script to create a list
+#   of samples in a directory and all of its
+#   subdirectories.
+
+usage() {
+    echo -e "\
+Usage: ./sample_list_generator.sh file_ext reads_dir out_dir out_name \n\
+where:  file_ext is the extenstion of samples being found \n\
+            example: \n\
+                .fastq.gz \n\
+                .txt.gz \n\
+                .fq.gz \n\
+\n\
+        reads_dir is the directory in which all samples or \n\
+            root directory for subdirectories containing samples \n\
+\n\
+        out_dir is the desired out directory for the list. \n\
+            NOTE: this should NOT be the same as reads_dir \n\
+\n\
+        out_name is the desired name for the sample list \n\
+" >&2
+    exit 1
+}
+
+if [ "$#" -lt 4 ]; then
+    usage;
+fi
+
+#   File extenstion of samples
+#       Example:
+#           .fastq.gz
+#           .txt.gz
+#           .fq.gz
+FILE_EXT="$1"
+
+#   Full path to directory in
+#   which ALL samples are stored
+#   NOTE: subdirectories for indivdidual
+#   samples may be within this directory.
+#   They will still be searched through
+READS_DIR="$2"
+
+#   Desired path and name of outfile
+#   Should not be same as $READS_DIR
+OUT_DIR="$3"
+OUT_NAME="$4"
+
+find "$READS_DIR" -name "*$FILE_EXT" > ${OUT_DIR}/${OUT_NAME}
