c9a7d7053771a16bad882b052a516f962bc84426
Paul Hoffman <hoffmanp@agro200953239.cfans.umn.edu>
2015-06-19 10:24:24 -0500

    Added instructions for Coverage_Map.sh

--- Coverage_Map.sh
+++ Coverage_Map.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-#PBS -l mem=12gb,nodes=1:ppn=1,walltime=4:00:00 
+#PBS -l mem=12gb,nodes=1:ppn=8,walltime=4:00:00 
 #PBS -m abe 
 #PBS -M user@example.com
 #PBS -q lab
@@ -11,26 +11,53 @@ set -o pipefail
 
 module load parallel
 
+#   This script is a qsub submission for read mapping a batch of files.
+#   To use, on line 5, change the 'user@example.com' to your own email address
+#       to get notifications on start and completion for this script
+#   Define a path to the bedtools in the 'BEDTOOLS' field on line 47
+#       If using MSI, leave the definition as is to use their installation of bedtools
+#       Otherwise, it should look like this:
+#           BEDTOOLS=${HOME}/software/bedtools
+#       Please be sure to comment out (put a '#' symbol in front of) the 'module load bedtools' line
+#       And to uncomment (remove the '#' symbol) from the 'BEDTOOLS=' line
+#   Add the full file path to list of samples on the 'SAMPLE_INFO' field on line 50
+#       This should look like:
+#           SAMPLE_INFO=${HOME}/Directory/list.txt
+#       Use ${HOME}, as it is a link that the shell understands as your home directory
+#           and the rest is the full path to the actual list of samples
+#   Name the project in the 'PROJECT' field on line 53
+#       This should look lke:
+#           PROJECT=Genetics
+#   Put the full directory path for the output in the 'SCRATCH' field on line 56
+#       This should look like:
+#           SCRATCH="${HOME}/Out_Directory"
+#       Adjust for your own out directory.
+#   Define a reference annotation file for the coverage mapping process in the 'REF_ANN' field on line 60
+#       This should look like:
+#           REF_ANN=${HOME}/Directory/annotation.bed
+#       This can be either a BED file or a GFF file
+#   Run this script using the qsub command
+#       qsub Coverage_Map.sh
+#   This script outputs a text file for each sample
+
 #   Path to BEDTools
 #   On MSI, loading the module works
 #   Otherwise, have path to BEDTools directory
 module load bedtools
 #BEDTOOLS=
 
-#   Scratch directory for output, 'scratch' is a symlink to individual user scratch at /scratch*
-SCRATCH=
-
-#   Name of Project
-#       Please wrap this in quotes
-#       Example: "SCN"
-PROJECT=
-
 #   List of BAM files for coverage mapping
 SAMPLE_INFO=
 
+#   Name of Project
+PROJECT=
+
+#   Scratch directory for output, 'scratch' is a symlink to individual user scratch at /scratch*
+SCRATCH=
+
 #   Annotation file to be considered
 #       This is the reference .bed or .gff file
-REF_ANN=${HOME}/Shared/Datasets/Annotations/Soybean_Cyst_Nematode/Heterodera_glycines_OP25_gene_models.gff
+REF_ANN=
 
 DATE=`date +%Y-%m-%d`
 
